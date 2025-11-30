c952959f96103359c3960d61064a531b2366df89
NJT123456 <jinjutha.namnjt@gmail.com>
2025-11-30 17:27:06 +0700

    create my portfolio

---
+++ src/app/components/nav.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from "react";
+import { useState } from "react";
+
+const Nav = () => {
+  const [currentSection, setCurrentSection] = useState<string>("/#home");
+
+  return <div>Nav</div>;
+};
+
+export default Nav;
